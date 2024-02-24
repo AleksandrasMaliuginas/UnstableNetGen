c4701b725382437b38fabc4529c29904aeb66718
AleksandrasMaliuginas <aleksandrasm348@gmail.com>
2024-02-24 17:36:14 +0100

    Introduce simple image transfer between Client and Server

---
+++ test-bench/SendingClient.py
@@ -0,0 +1,23 @@
+from network import Client
+from imageUtils import imageToBytes
+
+class SendingClient:
+
+    def __init__(self, client: Client, server_ip: str, server_port: int) -> None:
+        self.client = client
+        self.server_ip = server_ip
+        self.server_port = server_port
+
+    def start(self) -> None:
+        self.client.connect(self.server_ip, self.server_port)
+
+        self.doWork()
+
+    def doWork(self):
+        # Any sort of behavior of Sending Client
+        self.send_image()
+
+    def send_image(self) -> None:
+        bytesToSend = imageToBytes(0)
+        self.client.send(bytesToSend)
+        
---
+++ test-bench/start.py
@@ -0,0 +1,34 @@
+import socket, sys
+from PIL import Image
+from SendingClient import SendingClient
+from network.tcp import TCPServer, TCPClient
+from imageUtils import bytesToImage
+
+dgram_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+PORT = 1060
+
+if __name__ == "__main__":
+
+    if 2 <= len(sys.argv) <= 3 and sys.argv[1] == "server":
+        server_ip = sys.argv[2] if len(sys.argv) > 2 else ""
+
+        def image_handler(image_data: bytes):
+            img : Image = bytesToImage(image_data)
+            img.show('Received image')
+
+        server = TCPServer(image_handler)
+        server.start(server_ip, PORT)
+        server.listen()
+
+    elif len(sys.argv) == 3 and sys.argv[1] == "client":
+        server_ip = sys.argv[2]
+
+        SendingClient(
+            client=TCPClient(),
+            server_ip=server_ip,
+            server_port=PORT
+        ).start()
+
+    else:
+        print("Usage: <{client|server}> <server_ip>")
+
