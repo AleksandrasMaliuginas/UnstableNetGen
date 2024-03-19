c1982c558f8cd6fd329a5bfa06d65736855bae8f
AleksandrasMaliuginas <aleksandrasm348@gmail.com>
2024-03-19 21:50:22 +0100

    Refactor udp implementation

--- test-bench/network/udp.py
+++ test-bench/network/udp.py
@@ -1,28 +1,25 @@
 import socket
 from typing import Callable
 
-BUFF_SIZE = 1024
+MAX_BUFF_SIZE = 1024 * 1024
 
 
 class UDPServer:
 
-    def __init__(
-        self, server_hostname: str, port: int, requestHandler: Callable[[bytes], None]
-    ):
+    def __init__(self, requestHandler: Callable[[bytes], None]):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        self.server_hostname = server_hostname
-        self.port = port
         self.requestHandler = requestHandler
         self.closed = False
 
-    def start(self):
-        self.server_socket.bind((self.server_hostname, self.port))
+    def start(self, server_hostname: str, port: int):
+        self.server_socket.bind((server_hostname, port))
+        print(f"UDP socket open on port {port}.")
 
     def listen(self):
 
         while not self.closed:
-            message, address = self.server_socket.recvfrom(BUFF_SIZE)
+            message, address = self.server_socket.recvfrom(MAX_BUFF_SIZE)
 
             response = self.requestHandler(message)
 
@@ -42,12 +39,12 @@ class UDPClient:
 
         self.address = (server_hostname, port)
 
-    def send(self, message):
+    def send(self, message: bytes):
         self.client_socket.sendto(message, self.address)
 
     def awaitResponse(self, responseHandler: Callable[[bytes], None]) -> bool:
         try:
-            response, server = self.client_socket.recvfrom(1024)
+            response, server = self.client_socket.recvfrom(MAX_BUFF_SIZE)
             responseHandler(response)
             return True
         except socket.timeout:
