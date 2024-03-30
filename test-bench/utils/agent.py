1e6cd087dfaac427910a49a09c5d49de0855b044
AleksandrasMaliuginas <aleksandrasm348@gmail.com>
2024-03-30 22:28:53 +0100

    Introduce Agent implementation

---
+++ test-bench/utils/agent.py
@@ -0,0 +1,40 @@
+from threading import Thread
+from time import sleep
+
+
+class Agent:
+    def onStart(self) -> None:
+        pass
+
+    def doWork(self) -> int:
+        pass
+
+    def close(self) -> None:
+        pass
+
+
+class AgentRunner(Thread):
+    def __init__(self, agentName: str, periodicitySec: int, delegate: Agent):
+        Thread.__init__(self, name=agentName)
+
+        self.delegate = delegate
+        self.periodicitySec = periodicitySec
+
+        self.started = False
+        self.closed = False
+
+    def run(self):
+        if not self.started:
+            self.delegate.onStart()
+            self.started = True
+
+        while not self.closed:
+            workCount = self.delegate.doWork()
+            if workCount and workCount > 0:
+                continue
+
+            sleep(self.periodicitySec)
+
+    def close(self):
+        self.closed = True
+        self.delegate.close()
