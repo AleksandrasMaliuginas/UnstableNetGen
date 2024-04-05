0481f268b377fa77a6d2cb38db3ad26f6818467f
Henrik <theredjawa22@gmail.com>
2024-04-05 14:49:24 +0200

    can now split images before compressing

---
+++ Image_splitter.py
@@ -0,0 +1,78 @@
+from PIL import Image
+import os
+
+def split_image(image_path,out_path='', tile_size=1900):
+    try:
+        image = Image.open(image_path)
+    except FileNotFoundError:
+        print("Error: File not found.")
+        return
+    
+    width, height = image.size
+    num_tiles_horizontal = width // tile_size
+    num_tiles_vertical = height // tile_size
+    tiles = []
+
+    for i in range(num_tiles_horizontal):
+        for j in range(num_tiles_vertical):
+            left = i * tile_size
+            upper = j * tile_size
+            right = (i + 1) * tile_size
+            lower = (j + 1) * tile_size
+            tile = image.crop((left, upper, right, lower))
+            tile_path = f"tile_{i}_{j}.jpg"
+            tile_path = os.path.join(out_path, tile_path)
+            tile.save(tile_path)
+            tiles.append(tile_path)
+    #Getting the remainder on the bottom
+    left=0
+    right=width
+    upper=num_tiles_vertical*tile_size
+    lower=height
+    if lower-upper<100:
+        lower+=100
+    tile = image.crop((left, upper, right, lower))
+    tile_path = f"tile_{num_tiles_horizontal-1}_{num_tiles_vertical}.jpg"
+    tile_path = os.path.join(out_path, tile_path)
+    tile.save(tile_path)
+    tiles.append(tile_path)
+    #Getting the remainder on the right
+    left=num_tiles_horizontal*tile_size
+    upper=0
+    right=width
+    if right-left<100:
+        right+=100
+    lower=num_tiles_vertical*tile_size
+    tile = image.crop((left, upper, right, lower))
+    tile_path = f"tile_{num_tiles_horizontal}_{num_tiles_vertical-1}.jpg"
+    tile_path = os.path.join(out_path, tile_path)
+    tile.save(tile_path)
+    tiles.append(tile_path)
+    
+    
+    #num_tiles_horizontal+=1
+    #num_tiles_vertical+=1
+    return tiles, num_tiles_horizontal, num_tiles_vertical
+
+def merge_images(tiles, num_tiles_horizontal, num_tiles_vertical, output_path, final_res):
+    tile_size = Image.open(tiles[0]).size[0]
+    #width = (num_tiles_horizontal+1) * tile_size
+    #height = (num_tiles_vertical+1) * tile_size
+    width, height = final_res
+    new_image = Image.new("RGB", (width, height))
+ 
+    for i in range(num_tiles_horizontal):
+        for j in range(num_tiles_vertical):
+            index = i * num_tiles_vertical + j
+            if (index)>len(tiles)-1:
+                break
+            tile = Image.open(tiles[index])
+            new_image.paste(tile, (i * tile_size, j * tile_size))
+    index+=1
+    tile = Image.open(tiles[index])
+    new_image.paste(tile, (0, num_tiles_vertical*tile_size))
+    index+=1
+    tile = Image.open(tiles[index])
+    new_image.paste(tile, (num_tiles_horizontal*tile_size, 0))
+    new_image.save(output_path)
+    print(f"Image reassembled and saved at {output_path}")
--- blackbox.py
+++ blackbox.py
@@ -6,11 +6,16 @@ import collections
 from PIL import Image
 from compress import prepare_model, prepare_dataloader, compress_and_save, load_and_decompress
 import shutil
+import Image_splitter
 class HIFIC:
     def __init__(self, img_path, output_path,log_path, compression_level):
+        self.prepared='tobecompressed'
+        if not os.path.exists(self.prepared):
+            os.makedirs(self.prepared)
         self.img_path=img_path
         self.output_path=output_path
         self.logs=log_path
+        self.tile_size = 1900
         if compression_level>0 and compression_level<3:
             self.weights=compression_level
         else: self.weights=1
@@ -26,9 +31,18 @@ class HIFIC:
             self.weightpath='weights/hific_low.pt'
         print("hello")
         self.model, self.args = prepare_model(self.weightpath, self.logs) #output path is just to make a log
-        print("hello2")
-        self.data_loader=prepare_dataloader(self.args, self.img_path, self.output_path)
+        
     def compress(self):
+        for filename in os.listdir(self.img_path):
+            file_path = os.path.join(self.img_path, filename)
+            if os.path.isfile(file_path) and any(filename.lower().endswith(ext) for ext in ['.jpg', '.png']):
+                with Image.open(file_path) as img:
+                    width, height = img.size
+                    if width*height>3700000:
+                        Image_splitter.split_image(file_path, self.prepared, self.tile_size)
+                    else:
+                        shutil.copy(file_path, self.prepared)
+        self.data_loader=prepare_dataloader(self.args, self.prepared, self.output_path)
         compress_and_save(self.model, self.args, self.data_loader, self.output_path)
     def get_bpp(self, image_dimensions, num_bytes):
         w, h = image_dimensions
@@ -53,7 +67,12 @@ class HIFIC:
 #A new objects needs to be instantiated each time we need a different compression model, which takes a few seconds, so should be limited.
 #Because of how big the models are, system memory cannot fit more than one (from my tests)
 #Both sides need to instantiate the same model
-mycompressor=HIFIC(img_path='pepeimg',output_path='outputimg',log_path='logs', compression_level=3) 
+device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+mycompressor=HIFIC(img_path='pepeimg',output_path='outputimg',log_path='logs', compression_level=1) 
+#mycompressor1=HIFIC(img_path='pepeimg',output_path='outputimg',log_path='logs', compression_level=2) 
+#mycompressor2=HIFIC(img_path='pepeimg',output_path='outputimg',log_path='logs', compression_level=3) 
+#mycompressor1.model.to('cpu')
+#mycompressor2.model.to('cpu')
 #One side will use compress
 mycompressor.compress()
 #The other will use decompress
