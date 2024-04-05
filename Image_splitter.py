7369c8fda08d9cab90eb238ec8407eb750e224eb
Henrik <theredjawa22@gmail.com>
2024-04-05 16:35:48 +0200

    high resolution images are now automatically split and merged, and a dictionary called resolutions contains all the needed data for reconstruction

--- Image_splitter.py
+++ Image_splitter.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 
-def split_image(image_path,out_path='', tile_size=1900):
+def split_image(image_path,out_path='', tile_size=1900, split_number=0):
     try:
         image = Image.open(image_path)
     except FileNotFoundError:
@@ -20,38 +20,37 @@ def split_image(image_path,out_path='', tile_size=1900):
             right = (i + 1) * tile_size
             lower = (j + 1) * tile_size
             tile = image.crop((left, upper, right, lower))
-            tile_path = f"tile_{i}_{j}.jpg"
+            tile_path = f"tile{split_number}_{i}_{j}.jpg"   
             tile_path = os.path.join(out_path, tile_path)
             tile.save(tile_path)
             tiles.append(tile_path)
-    #Getting the remainder on the bottom
-    left=0
-    right=width
-    upper=num_tiles_vertical*tile_size
-    lower=height
-    if lower-upper<100:
-        lower+=100
-    tile = image.crop((left, upper, right, lower))
-    tile_path = f"tile_{num_tiles_horizontal-1}_{num_tiles_vertical}.jpg"
-    tile_path = os.path.join(out_path, tile_path)
-    tile.save(tile_path)
-    tiles.append(tile_path)
-    #Getting the remainder on the right
-    left=num_tiles_horizontal*tile_size
-    upper=0
-    right=width
-    if right-left<100:
-        right+=100
-    lower=num_tiles_vertical*tile_size
-    tile = image.crop((left, upper, right, lower))
-    tile_path = f"tile_{num_tiles_horizontal}_{num_tiles_vertical-1}.jpg"
-    tile_path = os.path.join(out_path, tile_path)
-    tile.save(tile_path)
-    tiles.append(tile_path)
+    if height>num_tiles_vertical*tile_size:
+        #Getting the remainder on the bottom
+        left=0
+        right=width
+        upper=num_tiles_vertical*tile_size
+        lower=height
+        if lower-upper<100:
+            lower+=100
+        tile = image.crop((left, upper, right, lower))
+        tile_path = f"tile{split_number}_{num_tiles_horizontal-1}_{num_tiles_vertical}.jpg"
+        tile_path = os.path.join(out_path, tile_path)
+        tile.save(tile_path)
+        tiles.append(tile_path)
+    if width>num_tiles_horizontal*tile_size:
+        #Getting the remainder on the right
+        left=num_tiles_horizontal*tile_size
+        upper=0
+        right=width
+        if right-left<100:
+            right+=100
+        lower=num_tiles_vertical*tile_size
+        tile = image.crop((left, upper, right, lower))
+        tile_path = f"tile{split_number}_{num_tiles_horizontal}_{num_tiles_vertical-1}.jpg"
+        tile_path = os.path.join(out_path, tile_path)
+        tile.save(tile_path)
+        tiles.append(tile_path)
     
-    
-    #num_tiles_horizontal+=1
-    #num_tiles_vertical+=1
     return tiles, num_tiles_horizontal, num_tiles_vertical
 
 def merge_images(tiles, num_tiles_horizontal, num_tiles_vertical, output_path, final_res):
--- blackbox.py
+++ blackbox.py
@@ -33,21 +33,31 @@ class HIFIC:
         self.model, self.args = prepare_model(self.weightpath, self.logs) #output path is just to make a log
         
     def compress(self):
+        #Checks every given image if it is the correct format and the size of the image, if its too large it iwll be split.
+        resolutions={}
+        split_images=0
         for filename in os.listdir(self.img_path):
             file_path = os.path.join(self.img_path, filename)
             if os.path.isfile(file_path) and any(filename.lower().endswith(ext) for ext in ['.jpg', '.png']):
                 with Image.open(file_path) as img:
                     width, height = img.size
                     if width*height>3700000:
-                        Image_splitter.split_image(file_path, self.prepared, self.tile_size)
+                        resolutions[split_images] = img.size
+                        Image_splitter.split_image(file_path, self.prepared, self.tile_size, split_images)
+                        split_images+=1
                     else:
                         shutil.copy(file_path, self.prepared)
         self.data_loader=prepare_dataloader(self.args, self.prepared, self.output_path)
         compress_and_save(self.model, self.args, self.data_loader, self.output_path)
+        for file_name in os.listdir(self.prepared):
+            file_path = os.path.join(self.prepared, file_name)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        return resolutions
     def get_bpp(self, image_dimensions, num_bytes):
         w, h = image_dimensions
         return num_bytes * 8 / (w * h)
-    def decompress(self):
+    def decompress(self, resolutions):
         File = collections.namedtuple('File', ['output_path', 'compressed_path',
                                        'num_bytes', 'bpp'])
         all_outputs = []
@@ -63,6 +73,40 @@ class HIFIC:
                                     compressed_path=compressed_file,
                                     num_bytes=os.path.getsize(compressed_file),
                                     bpp=self.get_bpp(Image.open(output_path).size, os.path.getsize(compressed_file))))
+        #Check for how many split images there are:
+        output_path='outputimg'
+        out_path = os.path.join(output_path, output_path)
+        file_count=0
+        tile_path = f"tile{file_count}_0_0"
+        split_imgs=[]
+        for root, dirs, files in os.walk(out_path):
+            for file_name in files:
+                if file_name.startswith(tile_path):
+                    file_count+=1
+                    tile_path = f"tile{file_count}_0_0"
+                    split_imgs.append(file_name)
+
+        for i in range(len(split_imgs)):
+            img_gatherer=[]
+            horizontal=0
+            vertical=0
+            for w in range(10):
+                horizontal_flag=0
+                for h in range(10):
+                    tile_path = f"tile{i}_{w}_{h}_compressed.png"
+                    img_path = os.path.join(out_path, tile_path)   
+                    if os.path.exists(img_path): 
+                        img_gatherer.append(img_path)
+                        if h>vertical:
+                            vertical=h 
+                        horizontal_flag+=1
+                    else:
+                        break
+                if horizontal_flag>0:
+                    horizontal+=1
+            resolution=resolutions[i]
+            img_name=os.path.join(out_path, f"img{i}.png")
+            Image_splitter.merge_images(tiles=img_gatherer, num_tiles_horizontal=horizontal-1, num_tiles_vertical=vertical, output_path=img_name, final_res=resolution)
 #print(os.system("nvidia-smi")) #I dont know why but sometimes its necessary to run this.
 #A new objects needs to be instantiated each time we need a different compression model, which takes a few seconds, so should be limited.
 #Because of how big the models are, system memory cannot fit more than one (from my tests)
@@ -74,8 +118,8 @@ mycompressor=HIFIC(img_path='pepeimg',output_path='outputimg',log_path='logs', c
 #mycompressor1.model.to('cpu')
 #mycompressor2.model.to('cpu')
 #One side will use compress
-mycompressor.compress()
+resolutions=mycompressor.compress()
 #The other will use decompress
-mycompressor.decompress()
+mycompressor.decompress(resolutions)
 #When a new model is to be used, use del to remove the previous one from memory
 del mycompressor
