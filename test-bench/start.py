9c08486d8a0bea8c8b049f8abb0c056c86b0813d
AleksandrasMaliuginas <aleksandrasm348@gmail.com>
2024-04-09 20:04:45 +0200

    Add logger

--- test-bench/start.py
+++ test-bench/start.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 from infrastructure.middle import AccessPoint
 from infrastructure.sender import SendingClient
 from compression import NoopEncoder
@@ -8,6 +9,8 @@ main_instance = None
 
 
 def main():
+
+    initLogging()
 
     if 2 <= len(sys.argv) <= 3 and sys.argv[1] == "server":
         server_ip = sys.argv[2] if len(sys.argv) > 2 else ""
@@ -27,6 +30,17 @@ def main():
 
     else:
         print("Usage: <{client|server}> <server_ip>")
+
+
+def initLogging():
+    logging.basicConfig(
+        filename="output.log",
+        level=logging.DEBUG,
+        format="%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    # Suppress PIL debug messages (https://github.com/ManimCommunity/manim/issues/363)
+    logging.getLogger("PIL").setLevel(logging.INFO)
 
 
 if __name__ == "__main__":
