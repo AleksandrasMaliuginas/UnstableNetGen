41a9a0fe13324b90df7f4bb33e3e0ac617bf5202
AleksandrasMaliuginas <aleksandrasm348@gmail.com>
2024-04-27 22:12:55 +0200

    Update TCP client&server

--- test-bench/network/tcp.py
+++ test-bench/network/tcp.py
@@ -1,90 +1,105 @@
-import socket
-from struct import pack, unpack
 import time
-from network import Client, SendBoundary
+import socket
+import socketserver
+from typing import Callable
+from struct import pack, unpack
+from network import NANOSECOND, Client, Server
+
+_requestHandler = None
+""" The request handler for received message.
+    Is instantiated on TCPServer creation. """
+
+
+def handleRequest(socket: socket.socket, dataReceived: bytes):
+
+    if _requestHandler is None:
+        return
+
+    response = _requestHandler(dataReceived)
+    if response:
+        socket.sendall(response)
+
+
+class TCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler for server.
+    Is instantiated once per connection to the server.
+    """
+
+    def setup(self):
+        self.receivedData = bytes()
+        self.remainingBytes = 0
+
+    def handle(self):
+        self.socket: socket.socket = self.request
+
+        while True:
+            if self.remainingBytes == 0:
+                header = self.socket.recv(8)
+
+                if len(header) == 0:
+                    return
+
+                requestLength = unpack(">xxLxx", header)[0]
+
+                self.remainingBytes = requestLength
+                self.receivedData = bytes()
+
+            data = self.socket.recv(min(1024 * 1024, self.remainingBytes))
+
+            self.receivedData += data
+            self.remainingBytes -= len(data)
+
+            if self.remainingBytes == 0:
+                handleRequest(self.socket, self.receivedData)
+
+            time.sleep(NANOSECOND)
+
+
+class TCPServer(Server):
+
+    def __init__(self, host: str, port: int, requestHandler: Callable[[bytes], (None | bytes)]) -> None:
+        global _requestHandler
+        _requestHandler = requestHandler
+        self.server = socketserver.TCPServer((host, port), TCPHandler)
+
+    def listen(self):
+        self.server.serve_forever(poll_interval=0.5)
+
+    def close(self):
+        self.server.shutdown()
+        self.server.server_close()
+        self.server = None
 
 
 class TCPClient(Client):
 
-    def connect(self, server_ip: str, server_port: str):
+    def __init__(self, host: str, port: int, timeout: int = 1) -> None:
+        self.address = (host, port)
+
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((server_ip, server_port))
+        self.socket.settimeout(timeout)
+
+        self.connected = False
+
+    def send(self, data: bytes):
+        if not self.connected:
+            self.socket.connect(self.address)
+            self.connected = True
+
+        header = pack(">xxLxx", len(data))
+        self.socket.sendall(header + data)
+
+    def awaitResponse(self, responseHandler: Callable[[bytes], None]) -> bool:
+        try:
+            response, address = self.socket.recvfrom(1024 * 1024)
+            responseHandler(response)
+            return True
+        except socket.timeout:
+            print("Request timed out.")
+            return False
 
     def close(self):
         self.socket.shutdown(socket.SHUT_WR)
+        self.connected = False
         self.socket.close()
-        self.socket = None
-
-    def send(self, data: bytes, boundary: SendBoundary = SendBoundary()):
-
-        # use struct to make sure we have a consistent endianness on the length
-        length = len(data)
-        expected_length = pack(">Q", length)
-
-        # sendall to make sure it blocks if there's back-pressure on the socket
-        self.socket.sendall(expected_length)
-
-        total_sent = 0
-        while total_sent < length:
-
-            sent = boundary.passOver(
-                data=data,
-                offset=total_sent,
-                next=lambda buffer: self.socket.send(buffer),
-            )
-
-            if sent == 0:
-                raise RuntimeError("socket connection broken")
-
-            total_sent += sent
-
-        ack = self.socket.recv(1)
-
-
-class TCPServer:
-
-    def __init__(self, requestHandler) -> None:
-        self.requestHandler = requestHandler
-
-    def start(self, server_ip, server_port):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((server_ip, server_port))
-        self.socket.listen(1)
-
-    def listen(self):
-        print("Listening...")
-        try:
-            while True:
-                (connection, addr) = self.socket.accept()
-                try:
-                    bs = connection.recv(8)
-                    (length,) = unpack(">Q", bs)
-                    data = b""
-
-                    start = time.time()
-                    while len(data) < length:
-                        # doing it in batches is generally better than trying
-                        # to do it all in one go, so I believe.
-                        to_read = length - len(data)
-                        data += connection.recv(4096 if to_read > 4096 else to_read)
-
-                    end = time.time()
-                    print(
-                        f"{length} bytes received in {end-start} sec.",
-                        f"BitRate: {length/(end-start)} bit/s.",
-                    )
-
-                    # send our 0 ack
-                    connection.sendall(b"\00")
-                finally:
-                    connection.shutdown(socket.SHUT_WR)
-                    connection.close()
-
-                self.requestHandler(data)
-
-        finally:
-            self.close()
-
-    def close(self):
-        self.socket.close()
-        self.socket = None
