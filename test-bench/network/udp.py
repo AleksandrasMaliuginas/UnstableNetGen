00c84b18c6993fe317e5ef83ab3806a9d23e3669
AleksandrasMaliuginas <aleksandrasm348@gmail.com>
2024-04-27 22:13:15 +0200

    Update TCP client&server implementation

--- test-bench/network/udp.py
+++ test-bench/network/udp.py
@@ -1,63 +1,115 @@
+import time
+import random
 import socket
+import socketserver
 from typing import Callable
+from struct import pack, unpack
+from network import NANOSECOND, Server, Client
 
-MAX_BUFF_SIZE = 1024 * 1024
-NANOSECOND = 1 / 1_000_000_000
+
+MAX_PACKET_SIZE = 32 * 1024
+
+_requestHandler = None
+""" The request handler for received message. Is instantiated on UDPServer creation. """
+_messages = dict()
+"""Received packets aggregates by messageId."""
 
 
-class UDPServer:
+class Message:
+    """Received packets aggregate."""
 
-    def __init__(self, requestHandler: Callable[[bytes], (None | bytes)]):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    def __init__(self, length: int):
+        self.length = length
+        self.data = bytes()
 
-        self.messageHandler = requestHandler
-        self.packetsReceived = []
-        self.closed = False
+    def append(self, data: bytes):
+        self.data += data
 
-    def start(self, server_hostname: str, port: int):
-        self.server_socket.bind((server_hostname, port))
-        print(f"UDP socket open on port {port}.")
+    def fullyReceived(self) -> bool:
+        return len(self.data) == self.length
+
+
+def handleRequest(request: socket.socket, client_address, dataReceived: bytes):
+
+    if _requestHandler is None:
+        return
+
+    response = _requestHandler(dataReceived)
+    if response:
+        request.sendto(response, client_address)
+
+
+def handleDatagram(request: socket.socket, client_address, dataReceived: bytes, messageId: int, length: int):
+    if not messageId in _messages:
+        _messages[messageId] = Message(length)
+
+    message: Message = _messages[messageId]
+    message.append(dataReceived)
+
+    if message.fullyReceived():
+        handleRequest(request, client_address, message.data)
+        del _messages[messageId]
+
+
+class UDPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler for server.
+    Is instantiated on every recv. Keeps no state.
+    """
+
+    def handle(self):
+        data, request = self.request
+
+        id, length = unpack(">xxLLxx", data[:12])
+
+        handleDatagram(request, self.client_address, data[12:], id, length)
+
+
+class UDPServer(Server):
+
+    def __init__(self, host: str, port: int, requestHandler: Callable[[bytes], (None | bytes)]) -> None:
+        global _requestHandler
+        _requestHandler = requestHandler
+        self.server = socketserver.UDPServer((host, port), UDPHandler)
 
     def listen(self):
-
-        while not self.closed:
-            message, address = self.server_socket.recvfrom(MAX_BUFF_SIZE)
-            self.packetsReceived.append((message, address))
-
-    def poll(self) -> int:
-        suppliedPacketCount = len(self.packetsReceived)
-
-        while suppliedPacketCount > 0:
-            message, address = self.packetsReceived.pop(0)
-            suppliedPacketCount -= 1
-
-            response = self.messageHandler(message)
-
-            if response:
-                self.server_socket.sendto(response, address)
-
-        return suppliedPacketCount
+        self.server.max_packet_size = MAX_PACKET_SIZE
+        self.server.serve_forever(poll_interval=0.5)
 
     def close(self):
-        self.closed = True
-        self.server_socket.close()
-        self.server_socket = None
+        self.server.shutdown()
+        self.server.server_close()
+        self.server = None
 
 
-class UDPClient:
+class UDPClient(Client):
 
-    def __init__(self, server_hostname: str, port: int, timeoutSeconds: float = 1.0):
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.client_socket.settimeout(timeoutSeconds)
+    def __init__(self, host: str, port: int, timeout: int = 1) -> None:
+        self.address = (host, port)
 
-        self.address = (server_hostname, port)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.settimeout(timeout)
 
-    def send(self, message: bytes):
-        self.client_socket.sendto(message, self.address)
+    def send(self, data: bytes):
+
+        messageId = random.randint(0, 4294967295)
+        messageLength = len(data)
+
+        while True:
+            header = pack(">xxLLxx", messageId, messageLength)
+            dataLength = min(len(data), MAX_PACKET_SIZE - len(header))
+
+            self.socket.sendto(header + data[:dataLength], self.address)
+
+            data = data[dataLength:]
+            if len(data) == 0:
+                break
+
+            time.sleep(NANOSECOND)
 
     def awaitResponse(self, responseHandler: Callable[[bytes], None]) -> bool:
         try:
-            response, server = self.client_socket.recvfrom(MAX_BUFF_SIZE)
+            response, address = self.socket.recvfrom(1024 * 1024)
             responseHandler(response)
             return True
         except socket.timeout:
@@ -65,6 +117,4 @@ class UDPClient:
             return False
 
     def close(self):
-        if self.client_socket:
-            self.client_socket.close()
-        self.client_socket = None
+        self.socket.close()
