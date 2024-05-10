f6b934d695c58633d81a1c949dcef197ff3b0df8
AleksandrasMaliuginas <aleksandrasm348@gmail.com>
2024-05-10 11:48:31 +0200

    Fix measurements do not use ping for measurements; Using iperf external library cli instead

--- test-bench/measurements/connection.py
+++ test-bench/measurements/connection.py
@@ -1,5 +1,7 @@
+import re
 import time
 import logging
+import subprocess
 from enum import Enum
 from utils.agent import Agent
 from network.udp import UDPClient
@@ -16,41 +18,32 @@ class ConnectionQuality:
     def reset(self):
         self.requestPacketCount = 0
         self.replyPacketCount = 0
-        self.bytesReceived = 0
+        self.bytesTransferred = 0
         self.totalRoundTripTime = 0
 
     def packetDropPercentage(self) -> float:
         return round(100 * (1 - self.replyPacketCount / self.requestPacketCount), 2)
 
     def dataTransferRateBytesPerSecond(self) -> float:
-        return round(self.bytesReceived / self.totalRoundTripTime, 2)
+        return round(self.bytesTransferred / self.totalRoundTripTime, 2)
     
-    def megaBits(self) -> float:
-        return round(self.bytesReceived / self.totalRoundTripTime * 8 / 1_000_000, 2)
+    def dataTransferRateMBps(self) -> float:
+        return round(self.bytesTransferred / self.totalRoundTripTime / 1_000_000, 2)
 
-    def roundTripTimeMs(self, precision: int = 2) -> float:
-        return round(1000 * self.totalRoundTripTime / self.replyPacketCount, precision)
+    def roundTripTimeSec(self) -> float:
+        return self.totalRoundTripTime
 
     def __str__(self):
         return (
             f"{self.__class__.__name__}: "
-            + f"megaBits={self.megaBits()}; "
             + f"requestPacketCount={self.requestPacketCount}; "
             + f"replyPacketCount={self.replyPacketCount}; "
-            + f"bytesReceived={self.bytesReceived}; "
+            + f"bytesReceived={self.bytesTransferred}; "
             + f"totalRoundTripTime={self.totalRoundTripTime}; "
             + f"packetDropPercentage={self.packetDropPercentage()}; "
             + f"dataTransferRateBytesPerSecond={self.dataTransferRateBytesPerSecond()}; "
-            + f"roundTripTimeMs={self.roundTripTimeMs()}; "
+            + f"roundTripTimeSec={self.roundTripTimeSec()}; "
         )
-
-
-class Counters(Enum):
-    PacketDropPercentage = 0
-    DataTransferRateBytesPerSecond = 1
-
-
-appCounters = AppCounters(Counters)
 
 
 class ConnectionObserver(Agent):
@@ -65,33 +58,56 @@ class ConnectionObserver(Agent):
         self.client = client
         self.measurePeriodSec = measurePeriodSec
         self.lastMeasurement = -1
-        self.pingPeriod = 0.1
-        self.pingRepetitions = 1000
+        self.pingRepetitions = 100
 
         self.connectionQuality = None
         self.records = {}
 
     def doWork(self):
-        if self.isTimeToMeasure():
-            self.measureConnectionQuality()
-            self.lastMeasurement = time.time()
 
-            appCounters.report(Counters.PacketDropPercentage, self.connectionQuality.packetDropPercentage())
-            appCounters.report(
-                Counters.DataTransferRateBytesPerSecond, self.connectionQuality.dataTransferRateBytesPerSecond()
-            )
-            # log.info(appCounters)
-            log.info(self.connectionQuality)
+        self.measureRSSI()
 
-    def isTimeToMeasure(self) -> bool:
-        return time.time() > self.lastMeasurement + self.measurePeriodSec
+        # if self.isTimeToMeasure():
+        #     self.measureConnectionQuality()
+        #     self.lastMeasurement = time.time()
 
-    def awaitInitialMeasurements(self, timeoutSec: int = 10):
-        start = time.time()
-        endAwaitTime = start + timeoutSec
+        #     appCounters.report(Counters.PacketDropPercentage, self.connectionQuality.packetDropPercentage())
+        #     appCounters.report(
+        #         Counters.DataTransferRateBytesPerSecond, self.connectionQuality.dataTransferRateBytesPerSecond()
+        #     )
+        #     # log.info(appCounters)
+        #     log.info(self.connectionQuality)
 
-        while self.connectionQuality == None and time.time() <= endAwaitTime:
-            time.sleep(0.5)
+    def measureRSSI(self):
+        process = subprocess.Popen("iwconfig", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        for line in process.stdout.readlines():
+            regex_signalStrength = re.compile(r"Signal level=(-\d+) dBm")
+            regex_linkQuality = re.compile(r"Link Quality=(\d+\/\d+)")
+
+            searchResult = regex_signalStrength.search(str(line))
+            if searchResult:
+                self.signalLevel = int(searchResult.groups()[0])
+
+            searchResult = regex_linkQuality.search(str(line))
+            if searchResult:
+                self.linkQuality = searchResult.groups()[0]
+
+        returnValue = process.wait()
+
+    #
+    #
+    #
+
+    # def isTimeToMeasure(self) -> bool:
+    #     return time.time() > self.lastMeasurement + self.measurePeriodSec
+
+    # def awaitInitialMeasurements(self, timeoutSec: int = 10):
+    #     start = time.time()
+    #     endAwaitTime = start + timeoutSec
+
+    #     while self.connectionQuality == None and time.time() <= endAwaitTime:
+    #         time.sleep(0.5)
 
     # TODO: Not thread safe
     def getConnectionQuality(self) -> ConnectionQuality:
@@ -101,9 +117,11 @@ class ConnectionObserver(Agent):
     def measureConnectionQuality(self):
         self.records.clear()
 
+        st = time.time()
         for i in range(1, 1 + self.pingRepetitions):
             self.sendPing(i)
             self.awaitPingResponse()
+        print("TIME:", time.time() - st)
 
         # TODO: Not thread safe
         connQuality = ConnectionQuality()
@@ -115,36 +133,45 @@ class ConnectionObserver(Agent):
         connQuality.requestPacketCount = requestPacketCount
 
         for seq, pingRecord in self.records.items():
-            request, start, end = pingRecord
+            request, start, pingReqLength, end, pingReplyLength = pingRecord
 
             if end:
                 connQuality.replyPacketCount += 1
-                connQuality.bytesReceived += self.PING_MESSAGE_SIZE_BYTES
+                connQuality.bytesTransferred += self.PING_MESSAGE_SIZE_BYTES * 2
                 connQuality.totalRoundTripTime += end - start
+        
+        # print("Speed:", round( / 1_000_000, 3))
+        print(connQuality.bytesTransferred, connQuality.totalRoundTripTime)
+        print(" Speed:", round(connQuality.bytesTransferred / connQuality.totalRoundTripTime / 1_000_000, 3))
 
     # Sending PING
     def sendPing(self, seq: int = 0):
         pingMessage = Ping(seq)
+        pingEncoded = pingMessage.encode()
 
         start = time.time()
-        self.client.send(pingMessage.encode())
+        self.client.send(pingEncoded)
 
-        self.records[seq] = (pingMessage, start, None)
+        self.records[seq] = (pingMessage, start, len(pingEncoded), None, None)
 
     # Receiving PING
     def awaitPingResponse(self):
         self.client.awaitResponse(self.onPingResponse)
 
     def onPingResponse(self, message):
+        # print("On resp message: ", len(message))
         end = time.time()
+        if len(message) != self.PING_MESSAGE_SIZE_BYTES:
+            return
+
         pingReply = Message.decode(message)
 
         if pingReply.msgType != MessageType.PING or pingReply.type != Ping.REPLY:
             return
 
         if pingReply.seq in self.records:
-            request, start, _ = self.records[pingReply.seq]
-            self.records[pingReply.seq] = (request, start, end)
-        
+            request, start, pingReqLength, _, _ = self.records[pingReply.seq]
+            self.records[pingReply.seq] = (request, start, pingReqLength, end, len(message))
+            # print((request, start, pingReqLength, end, len(message)))
 
         # print(f"{len(message)} bytes: ping_seq={pingReply.seq} time={roundTripDurationMs} ms")
